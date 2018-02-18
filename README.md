eb2af5910d6f0bd86d0d8bd07d9fd57733fcedbb
Fabio Bigmoby Mauro <fabio.mauro@databiz.it>
2018-02-18 15:10:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,120 @@
+_Based on work by benjaminion (https://github.com/ConsenSys/quorum-docker-Nnodes)_
+
+# Testing dockerized geth and constellation
+
+This tests the experimental setup to have geth (based on go-ethereum) and constellation run in separate docker instances, but in pairs such that each geth docker instance communicates with its corresponding constellation node docker instance via IPC instead of TCP.
+
+## Pre-requisite
+
+Build the docker images by launching
+
+```
+docker pull bigmoby/quorum
+```
+
+```
+docker pull bigmoby/constellation
+```
+
+from the project root. It should produce the following docker images:
+
+| Image                        | Description           | Needed for runtime? |
+| ---------------------------- |:---------------------:| -------------------:|
+| bigmoby/quorum         | geth node             | YES                 |
+| bigmoby/constellation  | constellation node    | YES                 |
+| bigmoby/quorum-builder | build environment     | NO                  |
+
+## Generate configuration artifacts and docker-compose.yaml
+
+```
+./setup.sh
+```
+
+The *setup.sh* script creates a basic Quorum network with Raft consensus. There's a whole bunch of things it needs to do in order to achieve this, some specific to Quorum, some common to private Ethereum chains in general.
+
+1. bootnode
+A bootnode is used in the network so that the geth nodes does not attempt to contact the well-known nodes in the public networks during p2p discovery. The script generates a node key for the bootnode and calculates its public address to be used in the geth node's `--bootnodes` argument.
+
+2. for each geth node
+The script generates all configuration files for the geth node in the `ethereum` folder. Inside the folder:
+
+ * *nodekey* file to uniquely identify this node on the network.
+ * *static-nodes.json* file that lists the Enode IDs of nodes that participate in the initial Raft cluster. Additional nodes can be added to the Raft cluster which is described [here](#adding-new-nodes-to-an-existing-network).
+ * *permissioned-nodes.json* file that captures the list of Enode IDs allowed to connect to each other in this network instance.
+ * Ether accounts are generated in the *keystore* directory
+   * The accounts get written into the *genesis.json* file with an initial balance
+
+3. for each constellation node
+ * the transaction manager's identity (key pair) in tm.pub and tm.key files
+ * the tm.conf file that configures the constellation node for url and listening port, and where other constellation nodes are
+
+4. docker-compose.yaml
+This makes it trivial to launch the network
+
+Refer to the *setup.sh* file itself for the full code.
+
+## Launch the network
+
+```
+docker-compose -f tmp/docker-compose.yaml up
+```
+
+## Advanced Topics
+
+### Configuration options
+
+Options are simple and self-explanatory. The *docker-compose.yml* file will create a Docker network for the nodes as per the `subnet` variable here. If you want to run more nodes, then add addresses for them to the `ips` list.
+
+    #### Configuration options #############################################
+
+    # One Docker container will be configured for each IP address in $ips
+    subnet="172.13.0.0/16"
+    ips=("172.13.0.3" "172.13.0.5" "172.13.0.7")
+
+
+**Note:** The setup.sh uses the IP addresses above for the quorum instances, the corresponding constellation instances will use the quorum's IP address minus 1 for its IP address. As a result, the list of IPs for the quorum nodes must be separated by at least 2.
+
+### Quorum configuration directory structure
+
+The configuration files for the geth and constellation nodes in Quorum are saved under the `ethereum` and `constellation` directories respectively:
+
+    /qdata/
+    ├── ethereum/
+    │   ├── geth/
+    │   ├── keystore/
+    │   │   └── UTC--2017-10-21T12-49-26.422099203Z--aad5479aff498c9258b21b59dd7546262aa2cfc7
+    │   ├── nodekey
+    │   ├── passwords.txt
+    │   ├── genesis.json
+    │   ├── static-nodes.json
+    │   └── permissioned-nodes.json
+    ├── constellation/
+    │   ├── tm.key
+    │   ├── tm.pub
+    │   └── tm.conf
+    └── logs/
+
+On the Docker host, a *qdata_N/* directory for each node is created with the structure above. When the network is started, this will be mapped by the *docker-compose.yml* file to each container's internal */qdata/* directory.
+
+### Adding new nodes to an existing network
+
+The docker image jpmorganchase/quorum uses a startup script that is designed to start the node in one of the following two modes:
+1. part of the initial Raft cluster. Notice in the docker-compose.yml file, the following command is used for this purpose:
+```
+command: start.sh --bootnode="enode://c3475a286a...6ebc6@172.13.0.100:30301" --raftInit
+```
+2. new node joining an existing network. You can modify the docker service start command as the following:
+```
+command: start.sh --bootnode="enode://c3475a286a...6ebc6@172.13.0.100:30301" --raftID=5
+```
+
+Note: the number `5` is the placeholder of the Raft node ID returned by calling `raft.addPeer()` in the geth console connected to an existing geth node of the network.
+
+### House-keeping
+
+Delete any old configuration.
+
+    ./scripts/cleanup.sh
+
+
+
---
+++ setup.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+
+#
+# Create all the necessary scripts, keys, configurations etc. to run
+# a cluster of N Quorum nodes with Raft consensus.
+#
+# The nodes will be in Docker containers. List the IP addresses that
+# they will run at below (arbitrary addresses are fine).
+#
+# Run the cluster with "docker-compose up -d"
+#
+# Run a console on Node N with "geth attach qdata_N/ethereum/geth.ipc"
+# (assumes Geth is installed on the host.)
+#
+# Geth and Constellation logfiles for Node N will be in qdata_N/logs/
+#
+
+# TODO: check file access permissions, especially for keys.
+
+
+#### Configuration options #############################################
+
+# One Docker container will be configured for each IP address in $ips
+subnet="172.13.0.0/16"
+ips=("172.13.0.3" "172.13.0.5" "172.13.0.7" "172.13.0.9" "172.13.0.11")
+
+cips=()
+## constellation node uses IP address derived from
+## the corresponding geth node IP address by subtracting 1
+for ip in ${ips[*]}
+do
+    [[ $ip =~ (.*\.)([0-9]+)$ ]] && cip=${BASH_REMATCH[2]}
+    cips+=("${BASH_REMATCH[1]}$(($cip - 1))")
+done
+
+# Docker image name
+image_constellation=bigmoby/constellation
+image_quorum=bigmoby/quorum
+
+########################################################################
+
+nnodes=${#ips[@]}
+
+if [[ $nnodes < 2 ]]
+then
+    echo "ERROR: There must be more than one node IP address."
+    exit 1
+fi
+
+./scripts/cleanup.sh
+
+cd tmp
+pwd=`pwd`
+
+#### Create directory for bootnode's configuration ##################
+echo '[0] Configuring for bootnode'
+
+mkdir qdata_0
+bootnode_cmd="docker run -it -v $pwd/qdata_0:/qdata $image_quorum /usr/local/bin/bootnode"
+$bootnode_cmd -genkey /qdata/nodekey
+bootnode_enode=`$bootnode_cmd -nodekey /qdata/nodekey -writeaddress | tr -d '[:space:]'`
+echo "bootnode id: $bootnode_enode"
+
+
+#### Create directories for each node's configuration ##################
+
+echo '[1] Configuring for '$nnodes' nodes.'
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+    mkdir -p $qd/{logs,constellation}
+    mkdir -p $qd/ethereum/geth
+
+    let n++
+done
+
+
+#### Make static-nodes.json and store keys #############################
+
+echo '[2] Creating Enodes and static-nodes.json.'
+
+echo "[" > static-nodes.json
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate the node's Enode and key
+    bootnode_cmd="docker run -it -v $pwd/$qd:/qdata $image_quorum /usr/local/bin/bootnode"
+    $bootnode_cmd -genkey /qdata/ethereum/nodekey
+    enode=`$bootnode_cmd -nodekey /qdata/ethereum/nodekey -writeaddress | tr -d '[:space:]'`
+    echo "Node $n id: $enode"
+
+    # Add the enode to static-nodes.json
+    sep=`[[ $n < $nnodes ]] && echo ","`
+    echo '  "enode://'$enode'@'$ip':30303?discport=0&raftport=50400"'$sep >> static-nodes.json
+
+    let n++
+done
+echo "]" >> static-nodes.json
+
+
+#### Create accounts, keys and genesis.json file #######################
+
+echo '[3] Creating Ether accounts and genesis.json.'
+
+cat > genesis.json <<EOF
+{
+  "alloc": {
+EOF
+
+n=1
+for ip in ${ips[*]}
+do
+    qd=qdata_$n
+
+    # Generate an Ether account for the node
+    touch $qd/ethereum/passwords.txt
+    create_account="docker run -v $pwd/$qd:/qdata $image_quorum /usr/local/bin/geth --datadir=/qdata/ethereum --password /qdata/ethereum/passwords.txt account new"
+    account1=`$create_account | cut -c 11-50`
+    account2=`$create_account | cut -c 11-50`
+    account3=`$create_account | cut -c 11-50`
+    account4=`$create_account | cut -c 11-50`
+    echo "Accounts for node $n: $account1 $account2 $account3 $account4"
+
+    # Add the account to the genesis block so it has some Ether at start-up
+    sep=`[[ $n < $nnodes ]] && echo ","`
+    cat >> genesis.json <<EOF
+    "${account1}": {
+      "balance": "1000000000000000000000000000"
+    }, "${account2}": {
+      "balance": "1000000000000000000000000000"
+    }, "${account3}": {
+      "balance": "1000000000000000000000000000"
+    }, "${account4}": {
+      "balance": "1000000000000000000000000000"
+    }${sep}
+EOF
+
+    let n++
+done
+
+cat >> genesis.json <<EOF
+  },
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "config": {
+    "homesteadBlock": 0
+  },
+  "difficulty": "0x0",
+  "extraData": "0x",
+  "gasLimit": "0x2FEFD800",
+  "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
+  "nonce": "0x0",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "timestamp": "0x00"
+}
+EOF
+
+
+#### Make node list for tm.conf ########################################
+
+nodelist=
+n=1
+for ip in ${cips[*]}
+do
+    sep=`[[ $ip != ${cips[0]} ]] && echo ","`
+    nodelist=${nodelist}${sep}'"http://'${ip}':9000/"'
+    let n++
+done
+
+
+#### Complete each node's configuration ################################
+
+echo '[4] Creating Quorum keys and finishing configuration.'
+
+n=1
+for ip in ${cips[*]}
+do
+    qd=qdata_$n
+
+    cat ../templates/tm.conf \
+        | sed s/_NODEIP_/${cips[$((n-1))]}/g \
+        | sed s%_NODELIST_%$nodelist%g \
+              > $qd/constellation/tm.conf
+
+    cp genesis.json $qd/ethereum/genesis.json
+    cp static-nodes.json $qd/ethereum/static-nodes.json
+    cp static-nodes.json $qd/ethereum/permissioned-nodes.json
+
+    # Generate Quorum-related keys (used by Constellation)
+    docker run -v $pwd/$qd:/qdata -v $pwd/../scripts:/scripts $image_constellation /scripts/generate-keys.sh
+    echo 'Node '$n' public key: '`cat $qd/constellation/tm.pub`
+
+    let n++
+done
+rm -rf genesis.json static-nodes.json
+
+
+#### Create the docker-compose file ####################################
+
+cat > docker-compose.yml <<EOF
+version: '2'
+services:
+  bootnode:
+    container_name: bootnode
+    image: $image_quorum
+    command: bootnode -nodekey /qdata/nodekey
+    volumes:
+      - './qdata_0:/qdata'
+    networks:
+      quorum_net:
+        ipv4_address: '172.13.0.100'
+
+EOF
+
+for index in ${!ips[*]}; do 
+    n=$((index+1))
+    qd=qdata_$n
+    ip=${ips[index]}; 
+    cip=${cips[index]}; 
+
+    cat >> docker-compose.yml <<EOF
+  constellation_$n:
+    container_name: constellation_$n
+    image: $image_constellation
+    volumes:
+      - './$qd:/qdata'
+    networks:
+      quorum_net:
+        ipv4_address: '$cip'
+    ipc: shareable
+
+  node_$n:
+    container_name: node_$n
+    image: $image_quorum
+    command: start.sh --bootnode="enode://$bootnode_enode@172.13.0.100:30301" --raftInit
+    volumes:
+      - './$qd:/qdata'
+    networks:
+      quorum_net:
+        ipv4_address: '$ip'
+    ports:
+      - $((n+22000)):8545
+    depends_on:
+      - constellation_$n
+      - bootnode
+    ipc: container:constellation_$n
+
+EOF
+done
+
+cat >> docker-compose.yml <<EOF
+
+networks:
+  quorum_net:
+    driver: bridge
+    ipam:
+      driver: default
+      config:
+      - subnet: $subnet
+EOF
